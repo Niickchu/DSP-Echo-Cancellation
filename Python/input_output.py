2f6c42fbf01dbd60d3db919c54a852e581d5bf56
Niick <niickchu@gmail.com>
2024-07-28 15:10:05 -0700

    add additional functions

--- Python/input_output.py
+++ Python/input_output.py
@@ -7,6 +7,11 @@ import numpy as np
 def get_current_folder():
     return os.path.dirname(os.path.abspath(__file__))
 
+def get_samples_folder_path():
+    return os.path.dirname(os.path.abspath(__file__)) + r"\samples"
+
+def get_output_folder_path():
+    return os.path.dirname(os.path.abspath(__file__)) + r"\output\\"
 
 def read_audio(file_path):
     signal, sample_rate = librosa.load(file_path, sr=None, mono=True)
