7555d52914888fdedfd5adf4b39ce54cf4ac815d
Cole Kashino <cjkashino@gmail.com>
2024-08-01 13:24:58 -0700

    cleaning for sub

--- MATLAB/effectiveness_quant.m
+++ MATLAB/effectiveness_quant.m
@@ -1,11 +1,10 @@
-mu = 0.001;                     % Step size
+mu = 0.01;                     % Step size
 del = 0.01;                     % Initial power
 lam = 0.98;                     % Averaging factor
-blockLength = 1024;
+blockLength = 2048;
 lowpass_cutoff = 2000;
 
-%%% Overall needs some fixing regarding strange noise in middle of audio
-
+%%
 
 % Read the audio signals
 sig_orig = audioread("mp3s/radio/CUT1-ORIG.mp3");
@@ -27,8 +26,6 @@ else
     sig_echo_padded = sig_echo_trimmed;
 end
 
-%% testing filtering
-
 % Create the frequency domain adaptive filter object
 hFDAF = dsp.FrequencyDomainAdaptiveFilter('Length', blockLength, ...
                                           'StepSize', mu, ...
@@ -37,8 +34,8 @@ hFDAF = dsp.FrequencyDomainAdaptiveFilter('Length', blockLength, ...
                                           'AveragingFactor', lam);
 
 % Compute the FFT of the padded signals
-X = fft(sig_orig_padded);
-D = fft(sig_echo_padded);
+D = fft(sig_orig_padded);
+X = fft(sig_echo_padded);
 
 % Apply the adaptive filter
 Y = hFDAF(X, D);
@@ -49,43 +46,55 @@ fs = 48000;
 y = ifft(Y);
 y = lowpass(y, lowpass_cutoff, fs);
 
-[mse_values, snr_values, rmse_values, erle_values, time_vector] = similarity_windows(sig_orig_padded, real(y), sig_echo_padded, fs, 1, 0);
-[mse_values2, snr_values2, rmse_values2, erle_values2, time_vector2] = similarity_windows(sig_orig_padded, sig_echo_padded, sig_echo_padded, fs, 1, 0);
-[corr, lags, mse, ssim_index, spec_sim, cosine_sim, ERLE, ERLE_mean] = similarity_stats(sig_orig_padded, real(y));
+[mse_values, snr_values, rmse_values, erle_values, time_vector] = similarity_windows(sig_orig_padded, real(y), sig_echo_padded, fs, 128, 0);
+[mse_values2, snr_values2, rmse_values2, erle_values2, time_vector2] = similarity_windows(sig_orig_padded, sig_echo_padded, sig_echo_padded, fs, 128, 0);
+
 
 time_signal = (0:length(sig_orig_padded)-1) / fs;
 
 x_end = max(time_vector);
-num_plots = 6;
 
-subplot(num_plots, 1, 1);
+figure;
+subplot(4, 1, 1);
 plot(time_signal, sig_orig_padded)
 title("Original Signal");
 xlim([0, x_end]);
 
-subplot(num_plots, 1, 2);
+subplot(4, 1, 2);
 plot(time_signal, sig_echo_padded)
 title("Echo Signal")
 xlim([0, x_end]);
 
-subplot(num_plots, 1, 3);
+subplot(4, 1, 3);
 plot(time_signal, real(y))
 title("Processed Signal")
 xlim([0, x_end]);
 
-subplot(num_plots, 1, 4);
-plot(time_vector, snr_values);
+subplot(4, 1, 4);
+plot(time_vector, erle_values);
+title("ERLE")
+xlim([0, x_end]);
+
+figure;
+
+subplot(4, 1, 1);
+plot(time_vector, abs(snr_values));
 title("SNR of Orig vs Processed")
 xlim([0, x_end]);
 
-subplot(6, 1, 5);
-plot(time_vector2, snr_values2);
+subplot(4, 1, 2);
+plot(time_vector2, abs(snr_values2));
 title("SNR of original vs echo")
 xlim([0, x_end]);
 
-subplot(num_plots, 1, 6);
-plot(time_vector, erle_values);
-title("ERLE")
+subplot(4, 1, 3);
+plot(time_vector, mse_values);
+title("MSE of Processed")
+xlim([0, x_end]);
+
+subplot(4, 1, 4);
+plot(time_vector, mse_values2);
+title("MSE of Echo")
 xlim([0, x_end]);
 
 p = audioplayer(real(y), fs);
