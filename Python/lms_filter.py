9dea601f1701e1786ecebe2e024fad27158f23a7
Niick <niickchu@gmail.com>
2024-08-03 14:14:51 -0700

    cleanup

--- Python/lms_filter.py
+++ Python/lms_filter.py
@@ -9,14 +9,14 @@ class LMS:
 
         self.folder = inout.get_current_folder()
 
-        if input_data == []:
+        if not len(input_data):
             mp3_file = self.folder + r"/samples/CUT1-ECHO.mp3"
             self.input_audio_data, self.input_sr = inout.read_audio(mp3_file)
         else:
             self.input_audio_data = input_data
             self.input_sr = sr
 
-        if desired_data == []:
+        if not len(desired_data):
             mp3_file = self.folder + r"/samples/CUT1-ORIG.mp3"
             self.desired_audio_data, _ = inout.read_audio(mp3_file)
         else:
@@ -30,10 +30,6 @@ class LMS:
         self.mu = None
         self.filter_order = None
 
-
-    def filter_freq_domain(self, input_data=None, desired_data=None, mu=None, filter_order=None):
-        pass
-
     def filter_time_domain(self, input_data=None, desired_data=None, mu=None, filter_order=None):
         
         if input_data == None:
@@ -43,13 +39,12 @@ class LMS:
             desired_data = self.desired_audio_data
 
         if mu == None:
-            self.mu = mu = 0.02
+            self.mu = mu = 0.15
 
         if filter_order == None:
-            self.filter_order = filter_order = 1024    #just for plotting
+            self.filter_order = filter_order = 256    #just for plotting
 
 
-        print("Data Length: " + str(len(input_data)) + "\tmu: " + str(mu) + "\tfilter order: " + str(filter_order))
 
         num_samples = len(input_data)
         weights = np.zeros(self.filter_order)
@@ -64,26 +59,26 @@ class LMS:
             e = np.zeros(num_samples)
             weights = np.zeros(self.filter_order)
 
-            # for n in range(0, num_samples - p):
-            #     x = input_data[n + p:n:-1]
-            #     y[n] = np.dot(weights, x)
-            #     e[n] = desired_data[n + p] - y[n]
-
-            #     weights = weights + mu * e[n] * x
-
-            #this prevents the time shifting
-            for n in range(p, num_samples): #start at filter order to avoid oob error with array
-                x = input_data[n-p:n]
+            for n in range(0, num_samples - p):
+                x = input_data[n + p:n:-1]
                 y[n] = np.dot(weights, x)
                 e[n] = desired_data[n] - y[n]
 
-                # #to prevent errors with audio clipping if weights are too large
-                # #if filtered sounds weird, then reduce mu or filter order
-                # weight_update = 2 * mu * e[n] * x
-                # if np.any(np.abs(weight_update) > 1):
-                #     weight_update = weight_update / np.max(np.abs(weight_update))
-
                 weights = weights + mu * e[n] * x
+
+            # #this prevents the time shifting
+            # for n in range(p, num_samples): #start at filter order to avoid oob error with array
+            #     x = input_data[n-p:n]
+            #     y[n] = np.dot(weights, x)
+            #     e[n] = desired_data[n] - y[n]
+
+            #     # #to prevent errors with audio clipping if weights are too large
+            #     # #if filtered sounds weird, then reduce mu or filter order
+            #     # weight_update = 2 * mu * e[n] * x
+            #     # if np.any(np.abs(weight_update) > 1):
+            #     #     weight_update = weight_update / np.max(np.abs(weight_update))
+
+            #     weights = weights + mu * e[n] * x
 
         self.done_time = inout.get_datetime_string()
         print("Done")
@@ -91,13 +86,6 @@ class LMS:
         output_file = self.folder + r"/output/" + self.done_time + "_time_filtered" + ".mp3"
         inout.save_audio(output_file, y, self.input_sr)
 
-
-        # lowpass_cutoff = 2000
-        # filtered_y = apply_lowpass_filter(y, lowpass_cutoff, self.input_sr)
-
-
-        # lowpass_output_file = self.folder + r"/output/" + self.done_time + "_lowpass_filtered" + ".mp3"
-        # inout.save_audio(lowpass_output_file, filtered_y, self.input_sr)
         return e, y, weights
     
     def get_metrics(self):
@@ -106,40 +94,40 @@ class LMS:
 if __name__ == "__main__":
     print("Creating Echo Signal...")
 
-    samples_folder = inout.get_samples_folder_path()
+    # samples_folder = inout.get_samples_folder_path()
 
-    filename = samples_folder + r"/female.wav"
-    x, sr  = librosa.load(filename,sr=8000)
-    filename = samples_folder + r"/male.wav"
-    d, sr  = librosa.load(filename,sr=8000)
+    # filename = samples_folder + r"/female.wav"
+    # x, sr  = librosa.load(filename,sr=8000)
+    # filename = samples_folder + r"/male.wav"
+    # d, sr  = librosa.load(filename,sr=8000)
 
-    rt60_tgt = 1.5   # desired reverberation time (the time in seconds for the energy to drop by 60 dB)
-    room_dim = [5, 5, 5]
+    # rt60_tgt = 1.5   # desired reverberation time (the time in seconds for the energy to drop by 60 dB)
+    # room_dim = [5, 5, 5]
 
-    e_absorption, max_order = pra.inverse_sabine(rt60_tgt, room_dim)
-    room = pra.ShoeBox(room_dim, fs=sr, materials=pra.Material(e_absorption), max_order=max_order)
-    room.add_source([1.5, 1.5, 1.5])
-    room.add_microphone([0.1, 0.5, 0.1])
-    room.compute_rir()
-    rir = room.rir[0][0]  #rir = room impulse response
-    rir = rir[np.argmax(rir):]  
+    # e_absorption, max_order = pra.inverse_sabine(rt60_tgt, room_dim)
+    # room = pra.ShoeBox(room_dim, fs=sr, materials=pra.Material(e_absorption), max_order=max_order)
+    # room.add_source([1.5, 1.5, 1.5])
+    # room.add_microphone([0.1, 0.5, 0.1])
+    # room.compute_rir()
+    # rir = room.rir[0][0]  #rir = room impulse response
+    # rir = rir[np.argmax(rir):]  
 
-    y = np.convolve(x,rir)  #x[n] * h[n] = echo component of final signal
-    scale = np.sqrt(np.mean(x**2)) /  np.sqrt(np.mean(y**2))
-    y = y*scale
+    # y = np.convolve(x,rir)  #x[n] * h[n] = echo component of final signal
+    # scale = np.sqrt(np.mean(x**2)) /  np.sqrt(np.mean(y**2))
+    # y = y*scale
 
-    L = max(len(y),len(d))
-    y = np.pad(y,[0,L-len(y)])
-    d = np.pad(d,[L-len(d),0])
-    x = np.pad(x,[0,L-len(x)])
+    # L = max(len(y),len(d))
+    # y = np.pad(y,[0,L-len(y)])
+    # d = np.pad(d,[L-len(d),0])
+    # x = np.pad(x,[0,L-len(x)])
 
-    output_folder = inout.get_output_folder_path()
-    filename = output_folder + r"/original_signal.wav"
-    inout.save_audio(filename, x, sr)
-    filename = output_folder + r"/echoed_signal.wav"
-    inout.save_audio(filename, y, sr)
+    # output_folder = inout.get_output_folder_path()
+    # filename = output_folder + r"/original_signal.wav"
+    # inout.save_audio(filename, x, sr)
+    # filename = output_folder + r"/echoed_signal.wav"
+    # inout.save_audio(filename, y, sr)
 
-    lms = LMS(y, x, sr)
+    # lms = LMS(y, x, sr)
     lms = LMS()
     error, output, weights = lms.filter_time_domain()
 
